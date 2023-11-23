62b70686cfaa13b36aedc70ba1d6371a7fbfd37d
usausa <machi.pon@gmail.com>
2023-11-23 21:42:34 +0900

    Fix inspection

--- Smart.Blazor/AspNetCore/Components/ScriptExtensions.cs
+++ Smart.Blazor/AspNetCore/Components/ScriptExtensions.cs
@@ -4,18 +4,12 @@ using Microsoft.JSInterop;
 
 public static class ScriptExtensions
 {
-    public static async ValueTask SetFocus(this IJSRuntime runtime, string id)
-    {
-        await runtime.InvokeAsync<string>("Smart.setFocus", id).ConfigureAwait(false);
-    }
+    public static ValueTask<string> SetFocus(this IJSRuntime runtime, string id) =>
+        runtime.InvokeAsync<string>("Smart.setFocus", id);
 
-    public static async ValueTask<string> SaveAsFile(this IJSRuntime runtime, string filename, string contentType, byte[] bytes)
-    {
-        return await runtime.InvokeAsync<string>("Smart.saveAsFile", filename, contentType, Convert.ToBase64String(bytes)).ConfigureAwait(false);
-    }
+    public static ValueTask<string> SaveAsFile(this IJSRuntime runtime, string filename, string contentType, byte[] bytes) =>
+        runtime.InvokeAsync<string>("Smart.saveAsFile", filename, contentType, Convert.ToBase64String(bytes));
 
-    public static async ValueTask<string> OpenNewWindow(this IJSRuntime runtime, string contentType, byte[] bytes)
-    {
-        return await runtime.InvokeAsync<string>("Smart.openNewWindow", contentType, Convert.ToBase64String(bytes)).ConfigureAwait(false);
-    }
+    public static ValueTask<string> OpenNewWindow(this IJSRuntime runtime, string contentType, byte[] bytes) =>
+        runtime.InvokeAsync<string>("Smart.openNewWindow", contentType, Convert.ToBase64String(bytes));
 }
