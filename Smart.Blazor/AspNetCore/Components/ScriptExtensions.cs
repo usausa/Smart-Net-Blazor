20a42f8e78c5e9c54ad3f8d6ac4a2fa8743cd576
usausa <machi.pon@gmail.com>
2025-11-30 19:03:51 +0900

    Update method

--- Smart.Blazor/AspNetCore/Components/ScriptExtensions.cs
+++ Smart.Blazor/AspNetCore/Components/ScriptExtensions.cs
@@ -4,8 +4,8 @@ using Microsoft.JSInterop;
 
 public static class ScriptExtensions
 {
-    public static ValueTask<string> SetFocus(this IJSRuntime runtime, string id) =>
-        runtime.InvokeAsync<string>("Smart.setFocus", id);
+    public static ValueTask SetFocus(this IJSRuntime runtime, string id) =>
+        runtime.InvokeVoidAsync("Smart.setFocus", id);
 
     public static ValueTask<string> SaveAsFile(this IJSRuntime runtime, string filename, string contentType, byte[] bytes) =>
         runtime.InvokeAsync<string>("Smart.saveAsFile", filename, contentType, Convert.ToBase64String(bytes));
